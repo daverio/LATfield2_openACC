ab2e406a96a813872c3739650832717626112497
Michel Mueller <muellermichel@github.com>
2015-07-06 16:45:55 +0200

    tests now execute two different problem sizes: 64 and 128

--- examples/RunTest.sh
+++ examples/RunTest.sh
@@ -7,14 +7,18 @@ script_dir="$( cd "$(dirname "$0")" ; pwd -P )"
 #parameters
 n_proc=2
 m_proc=2
-problem_size=128
+
+function run_test {
+	echo "Running $1 with problem size $2"
+	aprun -n ${total_proc} ${script_dir}/$1 -n ${n_proc} -m ${m_proc} -b $2
+}
 
 #setup
 total_proc=`expr $n_proc \* $m_proc`
 
 #run tests
-echo "Running fft_test"
-aprun -n ${total_proc} ${script_dir}/fft_test -n ${n_proc} -m ${m_proc} -b ${problem_size}
-echo "Running poisson"
-aprun -n ${total_proc} ${script_dir}/poisson -n ${n_proc} -m ${m_proc} -b ${problem_size}
+run_test fft_test 64
+run_test poisson 64
+run_test fft_test 128
+run_test poisson 128
 echo "All tests passed"
