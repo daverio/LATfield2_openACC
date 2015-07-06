e6a5f863e9d3cc966201377a947708383ef4f21f
David Daverio <david.daverio@unige.ch>
2015-07-06 14:27:30 +0200

    large temp

--- LATfield2_FFT_tempMem.hpp
+++ LATfield2_FFT_tempMem.hpp
@@ -80,13 +80,13 @@ temporaryMemFFT::~temporaryMemFFT()
 temporaryMemFFT::temporaryMemFFT(long size)
 {
 #ifdef SINGLE
-	temp1_ = (fftwf_complex *)fftwf_malloc(size*sizeof(fftwf_complex));
-	temp2_ = (fftwf_complex *)fftwf_malloc(size*sizeof(fftwf_complex));
+	temp1_ = (fftwf_complex *)fftwf_malloc(2*size*sizeof(fftwf_complex));
+	temp2_ = (fftwf_complex *)fftwf_malloc(2*size*sizeof(fftwf_complex));
 #endif
 	
 #ifndef SINGLE
-	temp1_ = (fftw_complex *)fftw_malloc(size*sizeof(fftw_complex));
-	temp2_ = (fftw_complex *)fftw_malloc(size*sizeof(fftw_complex));
+	temp1_ = (fftw_complex *)fftw_malloc(2*size*sizeof(fftw_complex));
+	temp2_ = (fftw_complex *)fftw_malloc(2*size*sizeof(fftw_complex));
 #endif
 	allocated_=size;
 }
@@ -99,8 +99,8 @@ int temporaryMemFFT::setTemp(long size)
 		fftwf_free(temp1_);
 		fftwf_free(temp2_);
         }
-		temp1_ = (fftwf_complex *)fftwf_malloc(size*sizeof(fftwf_complex));
-		temp2_ = (fftwf_complex *)fftwf_malloc(size*sizeof(fftwf_complex));
+		temp1_ = (fftwf_complex *)fftwf_malloc(2*size*sizeof(fftwf_complex));
+		temp2_ = (fftwf_complex *)fftwf_malloc(2*size*sizeof(fftwf_complex));
 		
 		//debug cout<<"("<< parallel.grid_rank()[0]<< ","<<parallel.grid_rank()[1] <<"): called temporary resize. Old size: " <<allocated_<<" , new size: "<< size<<endl;
 		
@@ -111,8 +111,8 @@ int temporaryMemFFT::setTemp(long size)
 		fftw_free(temp1_);
 		fftw_free(temp2_);
         }
-		temp1_ = (fftw_complex *)fftw_malloc(size*sizeof(fftw_complex));
-		temp2_ = (fftw_complex *)fftw_malloc(size*sizeof(fftw_complex));
+		temp1_ = (fftw_complex *)fftw_malloc(2*size*sizeof(fftw_complex));
+		temp2_ = (fftw_complex *)fftw_malloc(2*size*sizeof(fftw_complex));
 #endif
 		allocated_ = size ;
 	
