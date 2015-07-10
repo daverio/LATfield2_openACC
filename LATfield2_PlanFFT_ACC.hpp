ac47ee0b89b75ed6300e3555e812e6df2c3c89af
Michel Mueller <muellermichel@github.com>
2015-07-10 11:52:48 +0200

    moved transpositions to device

--- LATfield2_PlanFFT_ACC.hpp
+++ LATfield2_PlanFFT_ACC.hpp
@@ -769,15 +769,16 @@ void PlanFFT_ACC<compType>::execute_r2c_forward_dim0_tsp()
 {
 // !!!! there after the transpose not anymore same number of site in each core> last one have 1 addition layer, but r2cSizeLocal_ take care of it!!!
 
+#pragma acc update device(temp2_r2c_complex_[0:(r2cSizeLocal_ * rSize_[1]* rSizeLocal_[2]) ][0:2])
+#pragma acc data present(temp2_r2c_complex_[0:1], temp_r2c_complex_[0:1])
+{
     if(parallel.last_proc()[1])
     {
 	for(int i=0;i<parallel.grid_size()[1];i++)transpose_0_2_last_proc(&temp2_r2c_complex_[i*rSizeLocal_[1]*rSizeLocal_[2]*r2cSizeLocal_as_],&temp_r2c_complex_[i*rSizeLocal_[1]*rSizeLocal_[2]*r2cSizeLocal_],rSizeLocal_[1],rSizeLocal_[2],r2cSizeLocal_as_);
 	implement_local_0_last_proc(&temp2_r2c_complex_[rSize_[0]/2*rSizeLocal_[1]*rSizeLocal_[2]],temp_r2c_complex_,rSizeLocal_[1],rSizeLocal_[2],r2cSizeLocal_as_,parallel.grid_size()[1]);
     }
     else for(int i=0;i<parallel.grid_size()[1];i++)transpose_0_2(&temp2_r2c_complex_[i*rSizeLocal_[1]*rSizeLocal_[2]*r2cSizeLocal_as_],&temp_r2c_complex_[i*rSizeLocal_[1]*rSizeLocal_[2]*r2cSizeLocal_as_],rSizeLocal_[1],rSizeLocal_[2],r2cSizeLocal_as_);
-
-#pragma acc update device(temp_r2c_complex_[0:(r2cSizeLocal_ * rSize_[1]* rSizeLocal_[2]) ][0:2])
-
+}
 }
 
 template<class compType>
@@ -817,11 +818,14 @@ void PlanFFT_ACC<compType>::execute_r2c_forward_dim1_tsp()
 //with have dim2 in 0, dim0 in 1, dim1 in 2: so size = dim2size(rSize_[2]) * r2cSizeLocal_ * dim1size/procsize0(rSizeLocal_[2])
 // be extremly carefull!!!! as cubic we have dim1size/procsize0 = rSizeLocal_[2]
 
+	#pragma acc update device(temp_r2c_complex_[0:(r2cSizeLocal_ * rSize_[2]* rSizeLocal_[2]) ][0:2])
+	#pragma acc data present(temp2_r2c_complex_[0:1], temp_r2c_complex_[0:1])
+	{
     for(int i=0;i<parallel.grid_size()[0];i++)transpose_1_2(&temp_r2c_complex_[i*rSizeLocal_[2]*rSizeLocal_[2]*r2cSizeLocal_],
 							&temp2_r2c_complex_[i*rSizeLocal_[2]*rSizeLocal_[2]*r2cSizeLocal_],
 							r2cSizeLocal_,rSizeLocal_[2],rSizeLocal_[2]);
+    }
 
-#pragma acc update device(temp2_r2c_complex_[0:(r2cSizeLocal_ * rSize_[2]* rSizeLocal_[2]) ][0:2])
 
 }
 
@@ -1053,183 +1057,241 @@ void PlanFFT_ACC<compType>::execute_r2c_backward_d2h_send(int comp)
 
 #ifndef SINGLE
 
+#define DATA_SIZE_SEEN_BY_OPENACC 1
+#define DATA_SIZE_SEEN_BY_OPENACC_LAST 1
 
+/////
 template<class compType>
-void PlanFFT_ACC<compType>::transpose_0_2( fftw_complex * in, fftw_complex * out,int dim_i,int dim_j ,int dim_k)
+void PlanFFT_ACC<compType>::transpose_0_2( fftw_complex * in, fftw_complex * out,int dim_i,int dim_j ,int dim_k) //we need offset_in, offset_out everywhere
 {
-    int i,j,k;
-    for(i=0;i<dim_i;i++)
-    {
-	for(j=0;j<dim_j;j++)
+	int i,j,k;
+
+	#pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC], out[0:DATA_SIZE_SEEN_BY_OPENACC])
 	{
-	    for(k=0;k<dim_k;k++)
-	    {
+		#pragma acc loop independent
+		for(i=0;i<dim_i;i++)
+		{
+			#pragma acc loop independent
+			for(j=0;j<dim_j;j++)
+			{
+				#pragma acc loop independent
+				for(k=0;k<dim_k;k++)
+				{
 
-		out[k+dim_k*(j+i*dim_j)][0]=in[i+dim_i*(j+k*dim_j)][0];
-		out[k+dim_k*(j+i*dim_j)][1]=in[i+dim_i*(j+k*dim_j)][1];
+					out[k+dim_k*(j+i*dim_j)][0]=in[i+dim_i*(j+k*dim_j)][0];
+					out[k+dim_k*(j+i*dim_j)][1]=in[i+dim_i*(j+k*dim_j)][1];
 
-	    }
+				}
+			}
+		}
 	}
-    }
 
 }
 
 template<class compType>
 void PlanFFT_ACC<compType>::transpose_0_2_last_proc( fftw_complex * in, fftw_complex * out,int dim_i,int dim_j ,int dim_k)
 {
-    int i,j,k;
-    for(i=0;i<dim_i;i++)
-    {
-	for(j=0;j<dim_j;j++)
+	int i,j,k;
+	#pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC_LAST], out[0:DATA_SIZE_SEEN_BY_OPENACC_LAST])
 	{
-	    for(k=0;k<dim_k;k++)
-	    {
-		out[k+(dim_k+1)*(j+i*dim_j)][0]=in[i+dim_i*(j+k*dim_j)][0];
-		out[k+(dim_k+1)*(j+i*dim_j)][1]=in[i+dim_i*(j+k*dim_j)][1];
-	    }
+		#pragma acc loop independent
+		for(i=0;i<dim_i;i++)
+		{
+			#pragma acc loop independent
+			for(j=0;j<dim_j;j++)
+			{
+				#pragma acc loop independent
+				for(k=0;k<dim_k;k++)
+				{
+					out[k+(dim_k+1)*(j+i*dim_j)][0]=in[i+dim_i*(j+k*dim_j)][0];
+					out[k+(dim_k+1)*(j+i*dim_j)][1]=in[i+dim_i*(j+k*dim_j)][1];
+				}
+			}
+		}
 	}
-    }
 }
 
 template<class compType>
 void PlanFFT_ACC<compType>::implement_local_0_last_proc( fftw_complex * in, fftw_complex * out,int proc_dim_i,int proc_dim_j,int proc_dim_k,int proc_size)
 {
-    int i_in,i_out,j,rank;
-    for(i_in=0;i_in<proc_dim_i;i_in++)
-    {
-	for(j=0;j<proc_dim_j;j++)
+
+	int i_in,i_out,j,rank;
+	#pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC_LAST], out[0:DATA_SIZE_SEEN_BY_OPENACC_LAST])
 	{
-	    for(rank=0;rank<proc_size;rank++)
-	    {
-		i_out=i_in+rank*proc_dim_i;
-		out[proc_dim_k + (proc_dim_k+1)*(j+i_out*proc_dim_j)][0]=in[i_in+proc_dim_i*(j+proc_dim_j*rank)][0];
-		out[proc_dim_k + (proc_dim_k+1)*(j+i_out*proc_dim_j)][1]=in[i_in+proc_dim_i*(j+proc_dim_j*rank)][1];
-	    }
+		#pragma acc loop independent
+		for(i_in=0;i_in<proc_dim_i;i_in++)
+		{
+			#pragma acc loop independent
+			for(j=0;j<proc_dim_j;j++)
+			{
+				#pragma acc loop independent
+				for(rank=0;rank<proc_size;rank++)
+				{
+					i_out=i_in+rank*proc_dim_i;
+					out[proc_dim_k + (proc_dim_k+1)*(j+i_out*proc_dim_j)][0]=in[i_in+proc_dim_i*(j+proc_dim_j*rank)][0];
+					out[proc_dim_k + (proc_dim_k+1)*(j+i_out*proc_dim_j)][1]=in[i_in+proc_dim_i*(j+proc_dim_j*rank)][1];
+				}
+			}
+		}
 	}
-    }
 }
+
 
 template<class compType>
 void PlanFFT_ACC<compType>::transpose_1_2(fftw_complex * in , fftw_complex * out ,int dim_i,int dim_j ,int dim_k )
 {
-    int i,j,k;
-    for(i=0;i<dim_i;i++)
-    {
-	for(j=0;j<dim_j;j++)
+	int i,j,k;
+	#pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC], out[0:DATA_SIZE_SEEN_BY_OPENACC])
 	{
-	    for(k=0;k<dim_k;k++)
-	    {
-		out[i+dim_i*(k+j*dim_k)][0]=in[i+dim_i*(j+k*dim_j)][0];
-		out[i+dim_i*(k+j*dim_k)][1]=in[i+dim_i*(j+k*dim_j)][1];
-	    }
+		#pragma acc loop independent
+		for(i=0;i<dim_i;i++)
+		{
+			#pragma acc loop independent
+			for(j=0;j<dim_j;j++)
+			{
+				#pragma acc loop independent
+				for(k=0;k<dim_k;k++)
+				{
+					out[i+dim_i*(k+j*dim_k)][0]=in[i+dim_i*(j+k*dim_j)][0];
+					out[i+dim_i*(k+j*dim_k)][1]=in[i+dim_i*(j+k*dim_j)][1];
+				}
+			}
+		}
 	}
-    }
 }
 
 template<class compType>
-void PlanFFT_ACC<compType>::transpose_back_0_3( fftw_complex * in, fftw_complex * out,int r2c,int local_r2c,
-						int local_size_j,int local_size_k,int proc_size,int halo,int components, int comp)
+void PlanFFT_ACC<compType>::transpose_back_0_3( fftw_complex * in, fftw_complex * out,int r2c,int local_r2c,int local_size_j,int local_size_k,int proc_size,int halo,int components, int comp)
 {
-    int i,j,k,l, i_t, j_t, k_t;
-    int r2c_halo = r2c + 2*halo;
-    int local_size_k_halo = local_size_k + 2*halo;
-    for (i=0;i<local_r2c;i++)
-    {
-	for(k=0;k<local_size_k;k++)
-	{
-	    for(j=0;j<local_size_j;j++)
-	    {
-		for(l=0;l<proc_size;l++)
+	int i,j,k,l, i_t, j_t, k_t;
+	int r2c_halo = r2c + 2*halo;
+	int local_size_k_halo = local_size_k + 2*halo;
+	// #pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC]), present(out[0:DATA_SIZE_SEEN_BY_OPENACC])
+	// {
+	// 	#pragma acc loop independent
+		for (i=0;i<local_r2c;i++)
 		{
-		    i_t = i + l*local_r2c;
-		    j_t = j ;
-		    k_t = k ;
-		    out[comp+components*(i_t + r2c_halo * (k_t + local_size_k_halo * j_t))][0]=in[i + local_r2c * (j + local_size_j * (k + local_size_k*l)) ][0];
-		    out[comp+components*(i_t + r2c_halo * (k_t + local_size_k_halo * j_t))][1]=in[i + local_r2c * (j + local_size_j * (k + local_size_k*l)) ][1];
+			// #pragma acc loop independent
+			for(k=0;k<local_size_k;k++)
+			{
+				// #pragma acc loop independent
+				for(j=0;j<local_size_j;j++)
+				{
+					// #pragma acc loop independent
+					for(l=0;l<proc_size;l++)
+					{
+						i_t = i + l*local_r2c;
+						j_t = j ;
+						k_t = k ;
+						out[comp+components*(i_t + r2c_halo * (k_t + local_size_k_halo * j_t))][0]=in[i + local_r2c * (j + local_size_j * (k + local_size_k *l)) ][0];
+						out[comp+components*(i_t + r2c_halo * (k_t + local_size_k_halo * j_t))][1]=in[i + local_r2c * (j + local_size_j * (k + local_size_k *l)) ][1];
+					}
+				}
+			}
 		}
-	    }
-	}
-    }
+	// }
 }
 
 template<class compType>
 void PlanFFT_ACC<compType>::implement_0(fftw_complex * in, fftw_complex * out,int r2c_size,int local_size_j,int local_size_k, int halo,int components, int comp)
 {
-    int i,j,k;
-    i=r2c_size-1;
-    int r2c_halo = r2c_size + 2*halo;
-    int local_size_k_halo = local_size_k + 2*halo;
+	int i,j,k;
+	i=r2c_size-1;
+	int r2c_halo = r2c_size + 2*halo;
+	int local_size_k_halo = local_size_k + 2*halo;
 
-    for(j=0;j<local_size_j;j++)
-    {
-	for(k=0;k<local_size_k;k++)
-	{
-	    out[comp+components*(i + r2c_halo * (k + local_size_k_halo *j))][0]=in[j + local_size_j *k][0];
-	    out[comp+components*(i + r2c_halo * (k + local_size_k_halo *j))][1]=in[j + local_size_j *k][1];
-	}
-    }
-
+	// #pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC]), present(out[0:DATA_SIZE_SEEN_BY_OPENACC])
+	// {
+		// #pragma acc loop independent
+		for(j=0;j<local_size_j;j++)
+		{
+			// #pragma acc loop independent
+			for(k=0;k<local_size_k;k++)
+			{
+				out[comp+components*(i + r2c_halo * (k + local_size_k_halo *j))][0]=in[j + local_size_j *k][0];
+				out[comp+components*(i + r2c_halo * (k + local_size_k_halo *j))][1]=in[j + local_size_j *k][1];
+			}
+		}
+	// }
 }
+
 
 template<class compType>
 void PlanFFT_ACC<compType>::b_arrange_data_0(fftw_complex *in, fftw_complex * out,int dim_i,int dim_j ,int dim_k, int khalo, int components, int comp)
 {
-    int i,j,k;
-    int jump_i=(dim_i+ 2 *khalo);
-    int jump_j=dim_j+ 2 *khalo;
-    for(i=0;i<dim_i;i++)
-    {
-	for(j=0;j<dim_j;j++)
+	int i,j,k;
+	int jump_i=(dim_i+ 2 *khalo);
+	int jump_j=dim_j+ 2 *khalo;
+	#pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC]), present(out[0:DATA_SIZE_SEEN_BY_OPENACC])
 	{
-	    for(k=0;k<dim_k;k++)
-	    {
-		out[j + dim_j * (k + dim_k * i)][0]=in[comp+components*(i + jump_i * (j + jump_j*k))][0];
-		out[j + dim_j * (k + dim_k * i)][1]=in[comp+components*(i + jump_i * (j + jump_j*k))][1];
-	    }
+		#pragma acc loop independent
+		for(i=0;i<dim_i;i++)
+		{
+			#pragma acc loop independent
+			for(j=0;j<dim_j;j++)
+			{
+				#pragma acc loop independent
+				for(k=0;k<dim_k;k++)
+				{
+					out[j + dim_j * (k + dim_k * i)][0]=in[comp+components*(i + jump_i * (j + jump_j*k))][0];
+					out[j + dim_j * (k + dim_k * i)][1]=in[comp+components*(i + jump_i * (j + jump_j*k))][1];
+				}
+			}
+		}
 	}
-    }
-
 }
 
 template<class compType>
 void PlanFFT_ACC<compType>::b_transpose_back_0_1( fftw_complex * in, fftw_complex * out,int r2c,int local_r2c,int local_size_j,int local_size_k,int proc_size)
 {
-    int i,j,k,l, i_t, j_t, k_t;
+	int i,j,k,l, i_t, j_t, k_t;
 
-    for (i=0;i<local_r2c;i++)
-    {
-	for(k=0;k<local_size_k;k++)
+	#pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC]), present(out[0:DATA_SIZE_SEEN_BY_OPENACC])
 	{
-	    for(j=0;j<local_size_j;j++)
-	    {
-		for(l=0;l<proc_size;l++)
+		#pragma acc loop independent
+		for (i=0;i<local_r2c;i++)
 		{
-		    i_t = i + l*local_r2c;
-		    j_t = j ;
-		    k_t = k ;
-		    out[i_t + r2c * (k_t + local_size_k * j_t)][0]=in[i + local_r2c * (j + local_size_j * (k + local_size_k *l)) ][0];
-		    out[i_t + r2c * (k_t + local_size_k * j_t)][1]=in[i + local_r2c * (j + local_size_j * (k + local_size_k *l)) ][1];
+			#pragma acc loop independent
+			for(k=0;k<local_size_k;k++)
+			{
+				#pragma acc loop independent
+				for(j=0;j<local_size_j;j++)
+				{
+					#pragma acc loop independent
+					for(l=0;l<proc_size;l++)
+					{
+						i_t = i + l*local_r2c;
+						j_t = j ;
+						k_t = k ;
+						out[i_t + r2c * (k_t + local_size_k * j_t)][0]=in[i + local_r2c * (j + local_size_j * (k + local_size_k *l)) ][0];
+						out[i_t + r2c * (k_t + local_size_k * j_t)][1]=in[i + local_r2c * (j + local_size_j * (k + local_size_k *l)) ][1];
+					}
+				}
+			}
 		}
-	    }
 	}
-    }
 }
 
 template<class compType>
 void PlanFFT_ACC<compType>::b_implement_0(fftw_complex * in, fftw_complex * out,int r2c_size,int local_size_j,int local_size_k)
 {
-    int i,j,k;
-    i=r2c_size-1;
+	int i,j,k;
+	i=r2c_size-1;
 
 
-    for(j=0;j<local_size_j;j++)
-    {
-	for(k=0;k<local_size_k;k++)
+	#pragma acc kernels present(in[0:DATA_SIZE_SEEN_BY_OPENACC]), present(out[0:DATA_SIZE_SEEN_BY_OPENACC])
 	{
-	    out[i + r2c_size * (k + local_size_k *j)][0]=in[j + local_size_j *k][0];
-	    out[i + r2c_size * (k + local_size_k *j)][1]=in[j + local_size_j *k][1];
+		#pragma acc loop independent
+		for(j=0;j<local_size_j;j++)
+		{
+			#pragma acc loop independent
+			for(k=0;k<local_size_k;k++)
+			{
+				out[i + r2c_size * (k + local_size_k *j)][0]=in[j + local_size_j *k][0];
+				out[i + r2c_size * (k + local_size_k *j)][1]=in[j + local_size_j *k][1];
+			}
+		}
 	}
-    }
 
 }
 
